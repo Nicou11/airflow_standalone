86cd262d29af784e18e184a55be48d9c7156b1e0
Nicou11 <lyoungin1204@gmail.com>
2024-07-29 09:43:59 +0900

    vi

--- dags/movie.py
+++ dags/movie.py
@@ -7,6 +7,12 @@ from airflow import DAG
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import (
+    ExternalPythonOperator,
+    PythonOperator,
+    PythonVirtualenvOperator,
+    is_venv_installed,
+)
 
 with DAG(
     'movie',
@@ -15,12 +21,12 @@ with DAG(
     default_args={
         'depends_on_past': False,
         'retries': 1,
-        'retry_delay': timedelta(seconds=3)
+        'retry_delay': timedelta(minutes=5)
     },
     description='movie_db',
    # schedule_interval=timedelta(days=1),
     schedule="0 4 * * *",
-    start_date=datetime(2024, 7, 22),
+    start_date=datetime(2024, 7, 24),
     catchup=True,
     tags=['movie', 'movie_db'],
 ) as dag:
@@ -40,37 +46,11 @@ with DAG(
       #  == awk '{print "{{ds}}, "$2", "$1}' ${CNT_PATH} > ${CSV_PATH}/csv.csv
     )
 
-    task_create_table = BashOperator(
-        task_id= "create.table",
-        bash_command="""
-        """
-    )
+        #bash_command="""
+        #    echo "err report"
+        #""",
+        #trigger_rule="one_failed"
 
-    task_to_tmp = BashOperator(
-        task_id = "to.tmp",
-        bash_command="""
-        """
-    )
-
-    task_to_base = BashOperator(
-        task_id="to.base",
-        bash_command="""
-        """
-    )
-
-    task_make_done = BashOperator(
-        task_id="make.done",
-        bash_command="""
-        """
-    )
-
-    task_err = BashOperator(
-        task_id="err.report",
-        bash_command="""
-            echo "err report"
-        """,
-        trigger_rule="one_failed"
-    )
 
     task_end  = EmptyOperator(task_id='end', trigger_rule="all_done")
     task_start  = EmptyOperator(task_id='start')
