96abe67184e8ba9d7c967b3feb5236b0bf64ecf9
Nicou11 <lyoungin1204@gmail.com>
2024-07-29 15:47:53 +0900

    save

--- dags/movie.py
+++ dags/movie.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from textwrap import dedent
+from pprint import pprint
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -31,19 +32,46 @@ with DAG(
     tags=['movie', 'movie_db'],
 ) as dag:
 
-    task_get = BashOperator(
+    def print_context(ds, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        print("::group::All kwargs")
+        pprint(kwargs)
+        print(kwargs)
+        print("::endgroup::")
+        print("::group::Context variable ds")
+        print(ds)
+        print("::endgroup::")
+        return "Whatever you return gets printed in the logs"
+
+    def get_data(ds, **kwargs):
+        print(ds)
+        print(kwargs)
+        print("*" * 20)
+        print(f"ds_nodash => {kwargs['ds_nodash']}")
+        print(f"kwagrs type => {type(kwargs)}")
+        print("*" * 20)
+        from mov.api.call import get_key, save2df
+        key = get_key()
+        print(f"MOVIE_API_KEY => {key}")
+        YYYYMMDD = kwargs['ds_nodash']
+        df = save2df(YYYYMMDD)
+        print(df.head(5))
+
+    run_this = PythonOperator(
+            task_id="print_the_context",
+            python_callable=print_context)
+
+
+    get_data = PythonOperator(
         task_id='get.data',
-        bash_command="""
-            echo "get data" 
-    """
+        python_callable=get_data
     )
 
-    task_save = BashOperator(
+    save_data = BashOperator(
         task_id='save.data',
         bash_command="""
             echo "save data"
             """
-      #  == awk '{print "{{ds}}, "$2", "$1}' ${CNT_PATH} > ${CSV_PATH}/csv.csv
     )
 
         #bash_command="""
@@ -52,10 +80,11 @@ with DAG(
         #trigger_rule="one_failed"
 
 
-    task_end  = EmptyOperator(task_id='end', trigger_rule="all_done")
-    task_start  = EmptyOperator(task_id='start')
+    end  = EmptyOperator(task_id='end', trigger_rule="all_done")
+    start  = EmptyOperator(task_id='start')
 
-    task_start >> task_get >> task_save >> task_end
+    start >> get_data >> save_data >> end
+    start >> run_this >> end
 
 
 
