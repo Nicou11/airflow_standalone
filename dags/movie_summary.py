82fe27052fbae8123ebdd1c49e1d95a3b3d17e69
Nicou11 <lyoungin1204@gmail.com>
2024-08-01 17:57:42 +0900

    ing

--- dags/movie_summary.py
+++ dags/movie_summary.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from textwrap import dedent
-from pprint import pprint
+#from pprint import pprint as pp
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -35,15 +35,50 @@ with DAG(
     catchup=True,
     tags=['movie', 'movie_summary'],
 ) as dag:
+
+    REQUIREMENTS=["git+https://github.com/Nicou11/mov_agg@0.5/agg"]
+
+    def gen_empty(*ids):
+        tasks = []
+        for id in ids:
+            task = EmptyOperator(task_id=id)
+            tasks.append(task)
+        return tuple(tasks)
+
+
+    def gen_vpython(**kw):
+        task = PythonVirtualenvOperator(
+            task_id=kw['id'],
+            python_callable=kw['fun_obj'],
+            system_site_packages=False,
+            requirements=REQUIREMENTS,
+            op_kwargs=kw['op_kw'],
+            )
+        return task
     
-    def apply():
-        print(apply)
 
+    def pro_data(**params):
+        print(params['task_name'])
+        print(params) # task_name
+        print("@"*33)
 
-    def merge():
-        print(merge)
+    def pro_merge(task_name, **params):
+        load_dt = params['ds_nodash']
+        from mov_agg.util import merge
+        df = merge(load_dt)
+        print("*"*33)
+        print(df)
 
-    
+    def pro_data3(task_name):
+        print(task_name)
+        print("@"*33)
+
+    def pro_data4(task_name, ds_nodash, **kwargs):
+        print(task_name)
+        print(ds_nodash)
+        print(kwargs)
+        print("@"*33)
+
     def delete():
         print(delete)
 
@@ -52,37 +87,37 @@ with DAG(
         print(summary)
 
 
-    apply = PythonVirtualenvOperator(
-        task_id='apply_type.task',
-        python_callable=apply,
-        system_site_packages=False,
-    )
+    #start  = gen_empty(id='start')
+    #end  = gen_empty('end', trigger_rule="all_done")
+    start, end = gen_empty('start', 'end')
+    
+    apply_type = gen_vpython(
+            id = 'apply.type',
+            fun_obj = pro_data,
+            op_kw = {"task_name": "apply_type!!!"}
+            )
+
+    merge = gen_vpython(
+            id = 'merge.df',
+            fun_obj = pro_merge,
+            op_kw = 'load_dt'
+            )
+    
+    del_du = gen_vpython(
+            id = 'de.dup',
+            fun_obj = pro_data3,
+            op_kw = {"task_name": "del_du!!!"}
+            )
+
+    summary_df = gen_vpython(
+            id = "summary.df",
+            fun_obj = pro_data4,
+            op_kw = { "task_name": "summary_df!!!" }
+            )
 
 
-    merge = PythonVirtualenvOperator(
-        task_id='merge.task',
-        python_callable=merge,
-        system_site_packages=False,
-    )
-
-
-    del_du = PythonVirtualenvOperator(
-        task_id='del_du.task',
-        python_callable=delete,
-        system_site_packages=False,
-    )
-
-
-    summary = PythonVirtualenvOperator(
-        task_id='summary_df.task',
-        python_callable=summary,
-        system_site_packages=False,
-    )
-
-
-    end  = EmptyOperator(task_id='end', trigger_rule="all_done")
-    start  = EmptyOperator(task_id='start')
-
-    start >> apply >> merge >> del_du >> summary >> end
+    start >> merge
+    merge >> del_du >> apply_type
+    apply_type >> summary_df >> end
     
 
